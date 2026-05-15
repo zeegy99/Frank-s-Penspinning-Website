8d63719bb41e00b30d40b148b07bb7d88c9484f8
zeegy <fred.yuan392@gmail.com>
2026-05-14 23:20:41 -0500

    some more routes

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,25 @@ from flask import Flask, request, jsonify, session, make_response, redirect
 from flask_cors import CORS
 
 
+app = Flask(__name__)
+CORS(app, allow_headers=["Content-Type"], resources={r'/api/*': {"origins": "*"}},
+      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+@app.route('/')
+def index():
+    return "<h1>Hello World</h1>"
+@app.route('/order')
+def order():
+    print('yo gurt')
+    return "<h1>Hi</h1>"
+
+@app.route('/greet/<name>')
+def greet(name):
+
+    return f"<h1>Hello {name}</h1>"
+
+@app.route('/api/test')
+def test():
+    print("Test was hit")
+    return "<p>You hit the backend</p>"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555, debug=True)
--- frontend/src/homepage.jsx
+++ frontend/src/homepage.jsx
@@ -6,6 +6,11 @@ import { useNavigate } from "react-router-dom";
 function Homepage() {
     const navigate = useNavigate();
 
+    const test_api = async () => {
+        const response = await fetch("http://localhost:5555/api/test");
+        const data = await response.text();
+        console.log(data)
+    }
     const buy_pen = () => {
        window.open("https://buy.stripe.com/test_14AdR9b5h2NPcBR5cQenS01")
     }
@@ -16,6 +21,10 @@ function Homepage() {
 
         <button onClick={buy_pen}>
             Buy Toto's Pen
+        </button>
+
+        <button onClick={test_api}>
+            Test Backend
         </button>
         </>
     )
