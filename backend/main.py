4da41bf4b4a2e6aace13227c390102af3f065cba
zeegy <fred.yuan392@gmail.com>
2026-05-17 23:20:28 -0500

    email system

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify, session, make_response, redirect
 from flask_cors import CORS
 
+import os, smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 CORS(app, allow_headers=["Content-Type"], resources={r'/api/*': {"origins": "*"}},
       methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+
 @app.route('/')
 def index():
     return "<h1>Hello World</h1>"
+
 @app.route('/order')
 def order():
     print('yo gurt')
@@ -22,5 +27,45 @@ def greet(name):
 def test():
     print("Test was hit")
     return "<p>You hit the backend</p>"
+
+@app.route('/api/contact', methods=["POST"])
+def contact():
+
+    #Info comes in like this {'fname': 'asdf', 'email': 'asdf@gmail.com', 'content': 'asdf'}
+    data = request.get_json()
+
+    print(data)
+
+    full_name = data.get("fname")
+    email = data.get("email")
+    content=data.get("content")
+
+    sender = "fred.yuan392@gmail.com"
+    recipient = [email, "frank.toto2009@gmail.com"]
+    pwd = (os.getenv("APP_PASSWORD")).strip()
+
+    msg = MIMEMultipart("alternative")
+    msg["From"] = sender
+    msg["To"] = email
+    msg["Subject"] = "Penspinning Inquiry Confirmation Email"
+
+    txt = (
+        f"Hi {full_name},\n\n"
+         f"This is confirmation for what you asked: {content}\n\n"
+         "We will get back to you as soon as possible."
+    )
+
+    msg.attach(MIMEText(txt, "plain"))
+
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP("smtp.gmail.com", 587, timeout=30) as s:
+        s.ehlo(); s.starttls(context=context); s.ehlo()
+        s.login(sender, pwd)
+        s.sendmail(sender, recipient, msg.as_string())
+
+    print("this all worked")
+
+    return jsonify({"message": "If that email exists, we sent a reset code."}), 200
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
--- frontend/src/homepage.jsx
+++ frontend/src/homepage.jsx
@@ -5,14 +5,37 @@ import { useNavigate } from "react-router-dom";
 
 function Homepage() {
     const navigate = useNavigate();
+    const [email_confirmation, set_email_confirmation] = useState(false); 
 
     const test_api = async () => {
-        const response = await fetch("http://54.242.226.47:5555/api/test");
+        const response = await fetch("http://127.0.0.1:5555/api/test");
         const data = await response.text();
         console.log(data)
     }
     const buy_pen = () => {
        window.open("https://buy.stripe.com/test_14AdR9b5h2NPcBR5cQenS01")
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = 
+            {
+                fname: e.target.fname.value,
+                email: e.target.email.value,
+                content: e.target.content.value,
+            };
+        
+        const response = await fetch('http://127.0.0.1:5555/api/contact', 
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data),
+            });
+
+        if (response.ok) {
+            set_email_confirmation(true)
+        }
     }
 
     return (
@@ -30,6 +53,33 @@ function Homepage() {
         <button onClick={test_api}>
             Test Backend
         </button>
+
+        <p>Contact Us</p>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label for="fname">Full Name: </label>
+                    <input type="text" placeholder="Your Name" id="fname" required></input>
+                </div>
+                <div>
+                    <label for="email">Email Address: </label>
+                    <input type="email" placeholder="example@gmail.com" id="email" required></input>
+                </div>
+
+                <div>
+                     <label for="content">Inquiry: </label>
+                    <input type="text" placeholder="Your Inquiries" id="content" required></input>
+                </div>
+
+                <div>
+                    <input type="submit"></input>
+                </div>
+
+            </form>
+            {email_confirmation && (
+                <p>Successful, please check your email for confirmation</p>
+            )}
+        </div>
         </>
     )
 }
